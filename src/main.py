fb0c4d8531dee8542bc49dc07a27821e5981fa9f
lukasboehm97 <lukasboehm97@web.de>
2023-06-14 14:43:50 +0200

    add black formatting

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
-#%%
+# %%
 
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+
 # %%
 # Import train and test data from CSV
-df_total = pd.read_csv("../data/DailyDelhiClimate.csv", parse_dates=True, index_col="date")
+df_total = pd.read_csv(
+    "../data/DailyDelhiClimate.csv", parse_dates=True, index_col="date"
+)
 
 
 # %%
@@ -14,7 +17,7 @@ df_total.head()
 
 df_total.describe()
 
-#%%
+# %%
 # Visualization of input data before preparation
 
 fig = plt.figure(figsize=(30, 10))
@@ -23,7 +26,7 @@ columns = 2
 
 ax1 = fig.add_subplot(rows, columns, 1)
 ax1.set_title("Mean Temperature")
-plt.ylabel('Classes')
+plt.ylabel("Classes")
 df_total["meantemp"].plot()
 
 ax1 = fig.add_subplot(rows, columns, 2)
@@ -41,29 +44,28 @@ df_total["meanpressure"].plot()
 plt.show()
 
 
-#%%
+# %%
 # Data preparation
 
 ## Detection and removal of outliers
 q_low = df_total["meanpressure"].quantile(0.01)
-q_hi  = df_total["meanpressure"].quantile(0.99)
+q_hi = df_total["meanpressure"].quantile(0.99)
 
-df_filtered_total = df_total[(df_total["meanpressure"] < q_hi) & (df_total["meanpressure"] > q_low)]
+df_filtered_total = df_total[
+    (df_total["meanpressure"] < q_hi) & (df_total["meanpressure"] > q_low)
+]
 
 
-
-#%%
+# %%
 ##Normalization
-df_filtered_total['meanpressure'] = np.log2(df_filtered_total['meanpressure'])
+df_filtered_total["meanpressure"] = np.log2(df_filtered_total["meanpressure"])
 
 
-#%%
-#Standardization of data
+# %%
+# Standardization of data
 
 scaler = StandardScaler()
 
 standardized_data = scaler.fit_transform(df_filtered_total)
 
 print(standardized_data)
-
-# %%
